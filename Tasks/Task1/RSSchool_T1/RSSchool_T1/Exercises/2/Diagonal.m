948df0f931f290c0a41fdfbbb52792c32a7b2df2
Olga <volha.xx@gmail.com>
2019-03-24 22:09:57 +0300

    completed task diagonalDifference

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,24 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
-    return 0;
+    NSInteger summDiagonal1 = 0;
+    NSInteger summDiagonal2 = 0;
+    
+    for (int i = 0; i < array.count; i++) {
+        NSString* row = array[i];
+        NSArray* rowArray = [row componentsSeparatedByString:@" "];
+        
+        for (int j = 0; j < rowArray.count; j++) {
+            if (i == j) {
+                summDiagonal1 = summDiagonal1 + (( NSString *)rowArray[j]).integerValue;
+            }
+            if (j == rowArray.count - (i + 1)) {
+                summDiagonal2 = summDiagonal2 + (( NSString *)rowArray[j]).integerValue;
+            }
+        }
+    }
+    
+    return @(labs(summDiagonal2 - summDiagonal1));
 }
 
 @end
