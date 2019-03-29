0ee80232ec53886fb03a7968d2f4a658a8c95c0f
Olga <volha.xx@gmail.com>
2019-03-29 23:56:53 +0300

    Kidnapper task done

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,32 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+
+-(NSDictionary*)getWords:(NSString*)text {
+    NSArray* textComponents = [text.uppercaseString componentsSeparatedByString:@" "];
+    NSMutableDictionary* words = [NSMutableDictionary new];
+    for (NSString* word in textComponents) {
+        NSNumber* wordCount = [words objectForKey:word];
+        if (wordCount == nil) {
+            words[word] = @(1);
+        } else {
+            words[word] = @(wordCount.integerValue + 1);
+        }
+    }
+    return words;
+}
+
+- (BOOL)checkMagazine:(NSString *)magazine note:(NSString *)note {
+    NSDictionary* magWords = [self getWords:magazine];
+    NSDictionary* noteWords = [self getWords:note];
+    
+    for (NSString* word in noteWords.allKeys) {
+        NSNumber* countInNote = noteWords[word];
+        NSNumber* countInMagazine = [magWords objectForKey:word];
+        if (countInMagazine == nil || countInMagazine.integerValue < countInNote.integerValue) {
+            return NO;
+        }
+    }
+    return YES;
+}
 @end
