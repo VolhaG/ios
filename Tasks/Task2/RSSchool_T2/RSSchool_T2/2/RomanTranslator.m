94aa86aeedd4be273c40f0bac25c1f7c01a47df3
Olga <volha.xx@gmail.com>
2019-03-30 01:01:50 +0300

    + roman translator

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/2/RomanTranslator.m
@@ -1,5 +1,82 @@
 #import "RomanTranslator.h"
 
-@implementation RomanTranslator
-// your code here
+
+
+@implementation RomanTranslator {
+    NSDictionary* _romanValues;
+};
+
+-(NSDictionary*)romanValues {
+    if (_romanValues == nil) {
+        _romanValues = @{@"I" : @(1),
+                          @"IV" : @(4),
+                          @"V" : @(5),
+                          @"IX" : @(9),
+                          @"X" : @(10),
+                          @"XL" : @(40),
+                          @"L" : @(50),
+                          @"XC" : @(90),
+                          @"C" : @(100),
+                          @"CD" : @(400),
+                          @"D" : @(500),
+                          @"CM" : @(900),
+                          @"M" : @(1000)
+                          };
+    }
+    return _romanValues;
+}
+
+-(NSInteger)modifyRomanString:(NSMutableString*)romanString
+                    origValue:(NSInteger)origValue
+                        value:(NSInteger)value
+                   romanDigit:(NSString*)romanDigit
+{
+    NSInteger res = origValue;
+    while (res >= value) {
+        [romanString appendString:romanDigit];
+        res -= value;
+    }
+    return res;
+}
+
+- (NSString *)romanFromArabic:(NSString *)arabicString {
+    NSInteger value = arabicString.integerValue;
+    
+    NSMutableString* romanString = [NSMutableString new];
+    value = [self modifyRomanString:romanString origValue:value value:1000 romanDigit:@"M"];
+    value = [self modifyRomanString:romanString origValue:value value:900 romanDigit:@"CM"];
+    value = [self modifyRomanString:romanString origValue:value value:500 romanDigit:@"D"];
+    value = [self modifyRomanString:romanString origValue:value value:400 romanDigit:@"CD"];
+    value = [self modifyRomanString:romanString origValue:value value:100 romanDigit:@"C"];
+    value = [self modifyRomanString:romanString origValue:value value:90 romanDigit:@"XC"];
+    value = [self modifyRomanString:romanString origValue:value value:50 romanDigit:@"L"];
+    value = [self modifyRomanString:romanString origValue:value value:40 romanDigit:@"XL"];
+    value = [self modifyRomanString:romanString origValue:value value:10 romanDigit:@"X"];
+    value = [self modifyRomanString:romanString origValue:value value:9 romanDigit:@"IX"];
+    value = [self modifyRomanString:romanString origValue:value value:5 romanDigit:@"V"];
+    value = [self modifyRomanString:romanString origValue:value value:4 romanDigit:@"IV"];
+    value = [self modifyRomanString:romanString origValue:value value:1 romanDigit:@"I"];
+    
+    return romanString;
+}
+
+- (NSString *)arabicFromRoman:(NSString *)romanString {
+    long result = 0;
+    for (int i = 0; i < romanString.length; ++i) {
+        unichar curChar = [romanString characterAtIndex:i];
+        unichar nextChar = i == romanString.length - 1 ? 0 : [romanString characterAtIndex:i + 1];
+        NSNumber* value = nil;
+        if (nextChar != 0) {
+            value = [[self romanValues] objectForKey:[NSString stringWithFormat:@"%c%c", curChar, nextChar]];
+            if (value != nil) i++;
+        }
+        if (value == nil) {
+            value = [[self romanValues] objectForKey:[NSString stringWithFormat:@"%c", curChar]];
+        }
+        result += value.integerValue;
+    }
+    return [NSString stringWithFormat:@"%ld", result];
+
+}
+
 @end
