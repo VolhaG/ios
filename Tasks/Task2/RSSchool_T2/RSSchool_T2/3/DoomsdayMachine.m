d3046c273cf568fe90c3e3abc0dd520034f3e878
Olga <volha.xx@gmail.com>
2019-04-01 00:16:23 +0300

    added tasks 3-4

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/3/DoomsdayMachine.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/3/DoomsdayMachine.m
@@ -1,5 +1,85 @@
 #import "DoomsdayMachine.h"
 
+@interface AssimilationInfo : NSObject<AssimilationInfo>
+
+-(instancetype)initDateComponents:(NSDateComponents*)dateComponents;
+
+@property (nonatomic, readwrite) NSInteger years;
+@property (nonatomic, readwrite) NSInteger months;
+@property (nonatomic, readwrite) NSInteger weeks;
+@property (nonatomic, readwrite) NSInteger days;
+@property (nonatomic, readwrite) NSInteger hours;
+@property (nonatomic, readwrite) NSInteger minutes;
+@property (nonatomic, readwrite) NSInteger seconds;
+
+@end
+
+@implementation AssimilationInfo
+
+-(instancetype)initDateComponents:(NSDateComponents*)dateComponents {
+    self = [super init];
+    if (self != nil) {
+        self.years = dateComponents.year;
+        self.months = dateComponents.month;
+        self.weeks = dateComponents.weekOfYear;
+        self.days = dateComponents.day;
+        self.hours = dateComponents.hour;
+        self.minutes = dateComponents.minute;
+        self.seconds = dateComponents.second;
+    }
+    return self;
+}
+
+@end
+
+//Input: 2200:01:01@15\30/00
+
 @implementation DoomsdayMachine
-// your code here
+
+-(NSDate*)assimilationDate {
+//    14 August 2208 03:13:37
+    NSDateComponents* dateComponents = [NSDateComponents new];
+    dateComponents.year = 2208;
+    dateComponents.month = 8;
+    dateComponents.day = 14;
+    dateComponents.hour = 3;
+    dateComponents.minute = 12;
+    dateComponents.second = 37;
+    
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    return [gregorianCalendar dateFromComponents:dateComponents];
+}
+
+- (id<AssimilationInfo>)assimilationInfoForCurrentDateString:(NSString *)dateString {
+    NSCalendar *gregorianCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    
+    NSDateFormatter* dateFormatter = [NSDateFormatter new];
+    dateFormatter.dateFormat = @"yyyy:MM:dd@ss\\mm/hh";
+//    dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+    NSDate* date = [dateFormatter dateFromString:dateString];
+    
+    NSDateComponents* assimilationDateComponents = [NSDateComponents new];
+    assimilationDateComponents.year = 2208;
+    assimilationDateComponents.month = 8;
+    assimilationDateComponents.day = 14;
+    assimilationDateComponents.hour = 3;
+    assimilationDateComponents.minute = 12;
+    assimilationDateComponents.second = 37;
+    NSDate* assimilationDate = [gregorianCalendar dateFromComponents:assimilationDateComponents];
+    
+
+    NSDateComponents* dateComponents = [gregorianCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitWeekOfYear
+                                        |NSCalendarUnitDay|NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond
+                                                            fromDate:date
+                                                              toDate:assimilationDate
+                                                             options:0];
+
+    return [[AssimilationInfo alloc] initDateComponents:dateComponents];
+    
+}
+
+- (NSString *)doomsdayString {
+    return @"Sunday, August 14, 2208";
+}
+
 @end
--- Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
@@ -1,5 +1,23 @@
 #import "MatrixHacker.h"
 // your code here
-@implementation MatrixHacker
-// your code here
+
+typedef id<Character>(^InjectionBlock)(NSString*);
+
+@implementation MatrixHacker {
+    InjectionBlock injectedBlock;
+}
+
+- (void)injectCode:(id<Character> (^)(NSString *name))theBlock {
+    injectedBlock = theBlock;
+}
+
+- (NSArray<id<Character>> *)runCodeWithData:(NSArray<NSString *> *)names {
+    NSMutableArray<id<Character>> * res = [NSMutableArray new];
+    for (NSString* name in names) {
+        id<Character> character = injectedBlock(name);
+        [res addObject:character];
+    }
+    return res;
+}
+
 @end
